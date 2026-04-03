d1b64eb5221531550590cf1f42cb59f3d2985843
FFox3 <ffox211310@gmail.com>
2026-04-04 01:26:27 +0900

    Create daruma.js

---
+++ daruma.js
@@ -0,0 +1,70 @@
+let steps = 50;
+let isLooking = false;
+let darumaTimer;
+
+// 開始
+function startDarumaGame(){
+    steps = 50;
+    isLooking = false;
+
+    document.getElementById("steps").innerText = steps;
+    document.getElementById("sitom").src = "Haigo.png";
+    document.getElementById("statusText").innerText = "シトムくんが...";
+
+    nextTurn();
+}
+
+// タップ処理
+document.addEventListener("click", ()=>{
+    const screen = document.getElementById("darumaGame");
+
+    if(!screen || screen.style.display !== "flex") return;
+
+    if(isLooking){
+        endDaruma(false);
+    }else{
+        steps--;
+        document.getElementById("steps").innerText = steps;
+
+        if(steps <= 0){
+            endDaruma(true);
+        }
+    }
+});
+
+// ランダム振り向き
+function nextTurn(){
+    clearTimeout(darumaTimer);
+
+    const time = Math.random() * 2000 + 1000;
+
+    darumaTimer = setTimeout(()=>{
+        lookPlayer();
+    }, time);
+}
+
+// 見る
+function lookPlayer(){
+    isLooking = true;
+
+    document.getElementById("sitom").src = "SITMPlus.png";
+    document.getElementById("statusText").innerText = "見てるぞ！！";
+
+    setTimeout(()=>{
+        isLooking = false;
+        document.getElementById("sitom").src = "Haigo.png";
+        document.getElementById("statusText").innerText = "シトムくんが...";
+        nextTurn();
+    }, 800);
+}
+
+// 終了
+function endDaruma(clear){
+    clearTimeout(darumaTimer);
+
+    let text = clear ? "やるじゃないか" : "どんまい";
+
+    document.getElementById("darumaText").innerText = text;
+
+    showScreen("darumaResult");
+}
